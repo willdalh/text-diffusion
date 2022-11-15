4348e89a496c17e36795c17d94bf467239ad0b60
https://gitlab.stud.iie/williad <williad@stud.ntnu.no>
2022-11-15 10:50:13 +0100

    slicing dataset

--- src/run_training.py
+++ src/run_training.py
@@ -10,9 +10,11 @@ def run_training(args):
     text_denoiser.train()
     text_denoiser = text_denoiser.to(device)
 
+    torch.save(text_denoiser.vocab, args.log_dir + "/vocab.pt")
+
     for epoch in range(args.epochs):
-        loss = text_denoiser.run_epoch(device)
-        print(f"\nEpoch {epoch}: {loss}")
+        loss, comp_dict = text_denoiser.run_epoch(device)
+        print(f"\nEpoch {epoch} - Total loss: {loss}, Noise loss: {comp_dict['noise_loss']}, Reconstruction loss: {comp_dict['reconstruction_loss']}")
 
         if epoch % args.save_interval == 0 or epoch == args.epochs - 1 or epoch in [0, 1, 2, 3, 4, 5]:
             torch.save(text_denoiser.state_dict(), f"{args.log_dir}/models/saved_model.pt")
